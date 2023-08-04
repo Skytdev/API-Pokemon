b737bf219e1ede28689b9c0a526c6da1ea66f0ea
Yohan <yohan.pierrot_pro@outlook.com>
2023-08-04 03:24:50 +0200

    init

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Sharpi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private static List<Pokemon> PokemonObjectList = new List<Pokemon>();
+
+
+        [HttpPost]
+        [Route("/Pokemon/add", Name = "AddPokemon")]
+        public ActionResult Post([FromBody] Pokemon pokemon)
+        {
+            if (pokemon == null)
+            {
+                return BadRequest("Pokemon not found");
+            }
+
+            if (PokemonObjectList.Any(p => p.Id == pokemon.Id))
+            {
+                return Conflict("A Pokemon with this ID already exists.");
+            }
+
+            PokemonObjectList.Add(pokemon);
+
+            return CreatedAtAction(nameof(GetPokemonById), new { id = pokemon.Id }, pokemon);
+        }
+
+        [HttpPost]
+        [Route("/Pokemon/{id}/update", Name = "UpdatePokemon")]
+        public IActionResult UpdatePokemon([FromBody] Pokemon updatedPokemon, int id)
+        {
+            var pokemon = PokemonObjectList.FirstOrDefault(p => p.Id == id);
+
+
+            if (pokemon == null)
+            {
+                return NotFound("Pokemon not found");
+            }
+
+            //GetPokemons(PokemonObjectList, id, pokemon);
+            
+
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("/Pokemon/{id}", Name = "GetPokemonById")]
+        public ActionResult<Pokemon> GetPokemonById(int id)
+        {
+            var pokemon = PokemonObjectList.FirstOrDefault(p => p.Id == id);
+
+            if (pokemon == null)
+            {
+                return NotFound("Pokemon not found");
+            }
+
+            return pokemon;
+        }
+
+        [HttpGet]
+        [Route("/Pokemon/list", Name = "GetPokemonList")]
+        public IEnumerable<Pokemon> Get()
+        {
+            return PokemonObjectList;
+        }
+
+        [HttpDelete]
+        [Route("/Pokemon/{id}/delete", Name = "DeletePokemon")]
+        public IActionResult DeletePokemon(int id)
+        {
+            var pokemon = PokemonObjectList.FirstOrDefault(p => p.Id == id);
+
+            if (pokemon == null)
+            {
+                return NotFound("Pokemon not found");
+            }
+
+            PokemonObjectList.Remove(pokemon);
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Pokemon.cs
@@ -0,0 +1,14 @@
+﻿namespace Sharpi
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PokemonId { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Type { get; set; }
+        
+    }
+}
